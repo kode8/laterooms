93d22f6cd155015bb34b6c0242076394494dd3b1
John Carroll <john@kode8.com>
2018-03-08 19:33:53 +0000

    First draft of the search component styles

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
+// Assets
+import './Search.scss'
+
 class Search extends Component {
+
+  constructor() {
+    super();
+  }
+
   render() {
     return (
-      <div className="search-component" >
-        <h2 className="search-component__title">Here</h2>
-        <p>Test</p>
+      <div className="search-component">
+        <form method="post" action="/fallback" className="search-component__form">
+          <div className="search-component__form-control">
+            <label htmlFor="search" className="search-component__form-control-label">Search</label>
+            <input type="input" className="search-component__form-control-input" />
+            <button type="submit" className="search-component__form-control-btn" value="search">Submit</button>
+          </div>
+        </form> 
       </div>
     );
   }
