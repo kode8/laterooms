aa01642bdfde26336efb0dc218b430ddc10d9cb7
John Carroll <john@kode8.com>
2018-03-08 20:30:11 +0000

    Adding JS focus state
    Required when the input field isn’t empty and isn’t in focus

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -7,14 +7,31 @@ class Search extends Component {
 
   constructor() {
     super();
+
+    this.state = {
+      value: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleChange(e) {
+    this.setState(
+      { value: e.target.value }
+    );
+  }
+ 
   render() {
+    
+    const focus = (this.state.value !== "") 
+      ? 'search-component__form-control-input search-component__form-control-input--focus' 
+      : 'search-component__form-control-input';
+
     return (
       <div className="search-component">
         <form method="post" action="/fallback" className="search-component__form">
           <div className="search-component__form-control">
-            <input type="input" className="search-component__form-control-input" />
+            <input type="input" className={ focus } value={this.state.value} onChange={ this.handleChange } />
             <label htmlFor="search" className="search-component__form-control-label">Search</label>
             <button type="submit" className="search-component__form-control-btn" value="search">Submit</button>
           </div>
