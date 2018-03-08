cdcbe382e5695a5103c9381ca399d6bade695bb8
John Carroll <john@kode8.com>
2018-03-08 19:32:24 +0000

    Include core.scss into our main app

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import ReactDOM from 'react-dom';
 /* Import Components */ 
 import App from 'Containers/App';
 
+/* Import global scss */
+import 'Scss/core.scss';
+
 ReactDOM.render(<App />, document.getElementById('app'));
