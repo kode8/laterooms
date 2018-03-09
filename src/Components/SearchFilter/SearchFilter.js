6abc934568dae24e1ef4c90e79683de1cb48fbf4
John Carroll <john@kode8.com>
2018-03-09 12:23:56 +0000

    Search filter styles added

--- src/Components/SearchFilter/SearchFilter.js
+++ src/Components/SearchFilter/SearchFilter.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
+// Assets
+import './SearchFilter.scss'
+
 const SearchFilter = (props) => {
     return (
         <ul className="search-filter">
-             <li className="search-filter__item"><a href="#carpark" onClick={ props.onFilterChange('carpark') } >Car park</a></li>
-             <li className="search-filter__item"><a href="#gym"  onClick={ props.onFilterChange('gym') }>Gym</a></li>
+             <li className="search-filter__item">Filter: </li>
+             <li className="search-filter__item">
+                <a className="search-filter__link" href="#carpark" onClick={ props.onFilterChange('carpark') } >
+                    Car park
+                </a>
+            </li>
+            <li className="search-filter__item">
+                <a className="search-filter__link" href="#gym" onClick={ props.onFilterChange('gym') } >
+                    Gym
+                </a>
+            </li>
+            <li className="search-filter__item">
+                <a className="search-filter__link search-filter__link--dimmed" href="#gym" onClick={ props.onFilterChange('gym') } >
+                    Clear
+                </a>
+            </li>
         </ul>
     );
 }
