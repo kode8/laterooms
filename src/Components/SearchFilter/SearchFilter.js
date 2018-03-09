3b8a67a5ade30bfb5cf3048a6a459dc5fe82e007
John Carroll <john@kode8.com>
2018-03-09 13:31:20 +0000

    Fix auto firing of onClick event

--- src/Components/SearchFilter/SearchFilter.js
+++ src/Components/SearchFilter/SearchFilter.js
@@ -8,17 +8,22 @@ const SearchFilter = (props) => {
         <ul className="search-filter">
              <li className="search-filter__item">Filter: </li>
              <li className="search-filter__item">
-                <a className="search-filter__link" href="#carpark" onClick={ props.onFilterChange('carpark') } >
+                <a className="search-filter__link" href="#carpark" onClick={ () => { props.onFilterChange('car park') } } >
                     Car park
                 </a>
             </li>
             <li className="search-filter__item">
-                <a className="search-filter__link" href="#gym" onClick={ props.onFilterChange('gym') } >
+                <a className="search-filter__link" href="#gym" onClick={ () => { props.onFilterChange('gym') } } >
                     Gym
                 </a>
             </li>
             <li className="search-filter__item">
-                <a className="search-filter__link search-filter__link--dimmed" href="#gym" onClick={ props.onFilterChange('gym') } >
+                <a className="search-filter__link" href="#pool" onClick={ () => { props.onFilterChange('pool') } } >
+                    Pool
+                </a>
+            </li>
+            <li className="search-filter__item">
+                <a className="search-filter__link search-filter__link--dimmed" href="#all" onClick={ () => { props.onFilterChange('all') } } >
                     Clear
                 </a>
             </li>
